0b83b008dfb4157772506c05d29372621fc71ba0
Alvarosaguas <alvaro.saguas.yazyi@gmail.com>
2020-08-10 21:54:17 -0300

    Personaliza el saludo al usuario
    El cliente quiere que el saludo al usuario sea personalizado porque eso le va a traer ingresos extra

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream
-const nombreUsuario = prompt("Ingrese su nombre");
-alert(` Hola ${nombreUsuario}`);
 
-=======
+
 const nombreUsuario = prompt("Cual es tu nombre");
 alert(`Hola ${nombreUsuario}`);
->>>>>>> Stashed changes
+
